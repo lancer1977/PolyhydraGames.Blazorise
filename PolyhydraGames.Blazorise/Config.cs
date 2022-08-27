b5a55fec681b02952a4857a6d528a3d1265af8c0
Christopher Richmond <lancer1977@gmail.com>
2022-08-27 12:53:15 -0400

    Dialogs

---
+++ PolyhydraGames.Blazorise/Config.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+ 
+
+namespace PolyhydraGames.BlazorComponents
+{
+    /// <summary>
+    /// Extension methods for building the blazorise options.
+    /// </summary>
+    public static class Config
+    {
+        /// <summary>
+        /// Register blazorise and configures the default behaviour.
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        /// <param name="configureOptions"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddBlazorComponents( this IServiceCollection serviceCollection  )
+        {  
+            serviceCollection.AddSingleton<IDialogService, DialogService>();
+            return serviceCollection;
+        }
+    }
+}
